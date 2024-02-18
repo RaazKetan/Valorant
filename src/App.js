ff15d9bd155dba82b50782b838285367cacee770
Raaz <21ketanraaz@gmail.com>
2024-02-19 05:06:29 +0530

    Fetching Data From Valorant API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import AgentList from './components/AgentList';
 import './App.css';
 
 function App() {
+const [agents, setAgents] = useState([]);
 
  async function fetchAgentDetail(){
    const response = await fetch('https://valorant-api.com/v1/agents')
       const data = await response.json(); 
-      if(data.data[0].displayName === 'Gekko')
-    console.log(data.data[0].description);
-  }
+      console.log(data.data[0].role.displayName); 
+      const transformedAgents = data.data.map(agent => {
+        return {
+          uuid: agent.uuid,
+          name: agent.displayName,
+          description: agent.description,
+          image: agent.displayIcon,
+          // type: agent.role.map(role=>{return role.displayName})
+        }
+      });
+      setAgents(transformedAgents); 
+  };
   return (
    <React.Fragment>
       <Navbar />
@@ -18,6 +29,7 @@ function App() {
       <section>
         <button onClick={fetchAgentDetail}>Fetch Agents</button>
         </section>
+      <AgentList agents = {agents}/>
    </React.Fragment>
   );
 }
---
+++ src/components/AgentList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Agent from "./Agent";
+
+const AgentList = (props) => {
+    // console.log(props.agents[0])
+    return(
+        <ul>
+            {props.agents.map((agent) => (
+                <Agent
+                    key={agent.uuid}
+                    name={agent.name}
+                    description={agent.description}
+                    image={agent.image}
+                    // type={agent.type}
+                />
+            
+            ))}
+        </ul>
+    );
+};
+export default AgentList;
